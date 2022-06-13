6d280d2eeb9562f80a5e22a8ff9b13773c5df728
Abrar Nedal Alzubidi <a.alzubidi@ltuc.com>
2022-06-14 00:03:45 +0300

    finish lesson 54 from string functions

--- notes/string_functions.md
+++ notes/string_functions.md
@@ -142,3 +142,183 @@ echo str_replace(['ahmad','2000'], 'Done', $str); //Done,Done,issa,1000,fatima,D
 
 echo str_replace(['ahmad','2000'], ['@', '@@'], $str); //@,@@,issa,1000,fatima,@@,yosef,2600
 ```
+
+## Repeat String
+### What is it ??
+repeat a string for numbers of times. 
+
+This function returns a string that repeats number of times.
+### Syntax
+`str_repeat(string $string, int $times): string`
+
+which:
+- string: the string that want to repeat it.
+- times: the number of how much you want to repeat the string.
+- 0: not allowed as a value for number of times.
+
+### Example
+```
+$str = '@@@';
+echo str_repeat($str, 5); // @@@@@@@@@@@@@@@
+```
+## Shuffle String
+### What is it ??
+mix string charachters.
+
+### Syntax
+`str_shuffle(string $string): string`
+
+which:
+- string: the string that want to mix its element/char.
+### Example
+```
+echo str_shuffle('abrar'); //barra
+```
+
+## length String
+### What is it ??
+get the length of a string.
+
+### Syntax
+`strlen(string $string): number`
+
+which:
+- string: the string that want to get how many char have.
+### Example
+```
+echo strlen('(>w<)'); //5
+```
+
+## AddSlashes String
+### What is it ??
+Quote string with slashes.
+
+Returns a string with backslashes added before characters that need to be escaped. These characters are:
+- single quote (')
+- double quote (")
+- backslash (\)
+- NUL (the NUL byte)
+
+### Syntax
+`addslashes(string $string): string`
+
+which:
+- string: the string that want to be escaped.
+
+### important note:
+Never use addslashes function to escape values you are going to send to mysql. keep in mind that single quote is not the only special character that can break your sql query. and quotes are the only thing which addslashes care.
+
+### Example
+```
+$str_slash = "i' am blue :(  \ NULL";
+echo '<p>'. addslashes($str_slash) . '</p>'; # i\' am blue :( \\ NULL
+```
+
+## strip slashes String
+### What is it ??
+remove the backslashes inside a string.
+
+### Syntax
+`stripslashes($string: string): string`
+
+which:
+- string: the string that want to remove from it slashes.
+### Example
+```
+$str_slash = "i' am blue :(  \ NULL";
+echo stripslashes($str_slash); //i' am blue :( NULL
+```
+
+## strip tags String
+### What is it ??
+if allowed_tags not have any value it will remove html, xml and php tags from the string. and if it has a value if will keep those tags.
+
+### Syntax
+`strip_tags(string $string, array|string|null $allowed_tags = null): string`
+
+which:
+- string: the string that want to remove from it tags.
+- allowed_tags: allowed tags that will stay in the string.
+### Example
+```
+$str_slash = "i' am blue :(  \ NULL";
+echo stripslashes($str_slash); //i' am blue :( NULL
+```
+
+## Lower and upper String functions
+### What is it ??
+make string characters all of it in lower case or in upper case. or only the first word make the first char lower or upper. or each first char inside each word make it upper.
+
+### Syntax
+`strtolower(string $string): string`: lower case for all char's in the string.
+`strtoupper(string $string): string`: upper case for all char's in the string.
+`lcfirst(string $string): string`: lower case for only the first char inside the first word in the string.
+`ucfirst(string $string): string`: upper case for only the first char inside the first word in the string.
+`ucwords(string $string): string`: upper case only for the first char inside the  all words in the string.
+
+which:
+- string: the string that want to lower or upper it.
+### Example
+```
+$str = "my name is ABRAR";
+echo strtolower($str); //my name is abrar
+echo strtoupper($str); //MY NAME IS ABRAR 
+echo lcfirst($str); //my name is abrar
+echo ucfirst($str); //MY name is abrar 
+echo ucwors($str); //My Name Is Abrar 
+```
+
+## trim String
+### What is it ??
+remove the whitespaces and some special chars from the string.
+
+**special chars**:
+
+- " " (ASCII 32 (0x20)), an ordinary space.
+- "\t" (ASCII 9 (0x09)), a tab.
+- "\n" (ASCII 10 (0x0A)), a new line.
+- "\r" (ASCII 13 (0x0D)), a carriage return.
+- "\0" (ASCII 0 (0x00)), the NULL-byte.
+- "\v" (ASCII 11 (0x0B)), a vertical tab.
+
+### Syntax
+`trim(string $string, string $characters = " \n\r\t\v\x00"): string`
+
+which:
+- string: the string that want to remove from it tags.
+- characters: chars that you want to remove the default are above.
+### Example
+```
+$str = "  my name is abrar  ";
+echo var_dump($str); //20
+$trimed_str = trim($str);
+echo var_dump($trimed_str); //16
+```
+
+## string word count
+### What is it ??
+return how many words inside the string.
+
+### Syntax
+`str_word_count(string $string, int $format = 0, ?string $characters = null): array|int`
+
+which:
+- string: the string that want to count its words.
+- format: If the optional format is not specified, then the return value will be an integer representing the number of words found. In the event the format is specified, the return value will be an array, content of which is dependent on the format.
+- characters: A list of additional characters which will be considered as 'word'.
+- special chars and spaces are not counted.
+
+**format option**
+
+has 3 values:
+- 0: return the value as integer represent.
+- 1: return the value as array represent.
+- 2: return the value as associative array represent [this word at any index?] => [the word].
+### Example
+```
+$str = "my name is abrar";
+
+echo str_word_count($str, 0); //4
+echo print_r(str_word_count($str, 1)); //Array([0] => my, [1] => name, [2] => is, [3] => abrar)
+echo print_r(str_word_count($str, 2)); //Array([0] => my, [3] => name, [8] => is, [11] => abrar)
+```
--- string_functions.php
+++ string_functions.php
@@ -83,23 +83,105 @@ for($i = 0; $i<count($arr); $i++){
         array_push($salaries, $arr[$i]);
     }
 }
-function get_new_name($names){
-    $old_name = $_GET['oldName'];
-    $new_name = $_GET['newName'];
-    if(in_array($old_name, $names, true)){
-        $new_names = str_replace($old_name, $new_name, $names);
-        $new_arr = render_new_names($new_names);
-        return $new_arr;
-    }else{
-        echo '<div class="alert">';
-        echo '<span class="closebtn" onclick="this.parentElement.style.display="none";">&times;</span>';
-        echo '<strong>Error!</strong> the old name is not on the system.';
-        echo '</div>';
-    }
+echo '<table style="margin-bottom:10px">';
+echo '<tr>';
+echo '<th>Employee Name</th>';
+echo '<th>Salary</th>';
+echo '</tr>';
+for($i = 0; $i<count($names); $i++){
+  echo '<tr>';
+  echo '<td>' . $names[$i] . '</td>';
+  echo '<td>' . $salaries[$i] . '</td>';
+  echo '</tr>';
 }
-function render_new_names($names){
-    return $names;
-}
+echo '</table>';
+// =============END ==============
+
+// ========= str_repeat function =========
+echo '<h2>repeat string function</h2>';
+$repeate_string = ' \(≧∇≦)/ (╯°□°）╯    ';
+echo '<p>'. str_repeat($repeate_string, 5) . '</p>';
+// ========= END ===================
+
+// =========== str_shuffle function =========
+echo '<h2>shuffle string function</h2>';
+$shuffle_string = '(>w<)';
+echo '<p>'. str_shuffle($shuffle_string) . '</p>';
+// ========= END =================
+
+// =========== strlen function =========
+echo '<h2>length string function</h2>';
+echo '<p>'. "(>w<) length is: " .  strlen('(>w<)') . '</p>';
+// ========= END =================
+
+// =========== addslash function =========
+echo '<h2>add slashes string function</h2>';
+$str_slash = "i' am blue :(  \ NULL";
+echo '<p> normal string is ***** '. $str_slash . ' *****</p>';
+echo '<p> after add slash function *****  '. addslashes($str_slash) . ' *****</p>';
+// ========= END =================
+
+// =========== stripslashes function =========
+echo '<h2>strip slashes string function</h2>';
+$str_slash = "i' am blue :(  \ NULL";
+echo '<p> normal string is ***** '. $str_slash . ' *****</p>';
+echo '<p> after add slash function *****  '. addslashes($str_slash) . ' *****</p>';
+echo '<p> after clean it from slashs by strip slashes function *****  '. stripslashes($str_slash) . ' *****</p>';
+// ========= END =================
+
+// =========== striptags function =========
+echo '<h2>strip tags string function</h2>';
+$str_slash = "go to <a href='https://google.com'> <b>Google</b></a> to Search about <span style='color:green'>anything</span>";
+echo '<p> normal string is ***** '. $str_slash . ' *****</p>';
+echo '<p> after implement strip tags without allowed_tags properity *****  '. strip_tags($str_slash) . ' *****</p>';
+echo '<p> after implement strip tags with allowed_tags properity *****  '. strip_tags($str_slash, "<a><span>") . ' *****</p>';
+// ========= END =================
+
+// =========== strtolower, strtoupper, lcfirst, ucfirst and ucwords function =========
+echo '<h2>lower & upper string functions</h2>';
+$str = "my name is ABRAR";
+echo '<p> normal string is ***** '. $str . ' *****</p>';
+echo '<p> after implement lower string *****  '. strtolower($str) . ' *****</p>';
+echo '<p> after implement upper string *****  '. strtoupper($str) . ' *****</p>';
+echo '<p> after implement lower case first *****  '. lcfirst($str) . ' *****</p>';
+echo '<p> after implement upper case first *****  '. ucfirst($str) . ' *****</p>';
+echo '<p> after implement upper case words *****  '. ucwords($str) . ' *****</p>';
+// ========= END =================
+
+// =========== trim function =========
+echo '<h2>trim string function</h2>';
+$str = "my name is abrar";
+echo '<p> normal string is ***** '. $str . ' ***** and its length '. strlen($str) .' </p>';
+$str = "  my name is abrar     ";
+echo '<p> after add 5 spaces, 2 at left and 3 at right *****  '. $str . ' ***** and its length '. strlen($str) .' </p>';
+echo '<p> after implement trim *****  '. $str . ' ***** and its length '. strlen(trim($str)) .' </p>';
+echo '<p> after implement left trim *****  '. $str . ' ***** and its length '. strlen(ltrim($str)) .' </p>';
+echo '<p> after implement right trim *****  '. $str . ' ***** and its length '. strlen(rtrim($str)) .' </p>';
+$trimed_str = trim($str, "my nar");
+echo '<p> after implement trim with special chars *****  '. $trimed_str . ' ***** and its length '. strlen(trim($str, "y n")) .' </p>';
+// ========= END =================
+
+// =========== string word count function =========
+echo '<h2>string word count function</h2>';
+$str = "my name is abrar  / ++ ====";
+echo '<p> normal string is ***** '. $str . ' ***** and its length (by words) is '. str_word_count($str, 0) .' </p>';
+echo '<p> ▶ after implement str_word_count with format=1 ***** </p>';
+echo '<pre>';
+echo print_r(str_word_count($str, 1));
+echo '<p> and its length (by words) is '. str_word_count($str) .' </p>';
+echo '</pre>';
+echo '<p> ▶ after implement str_word_count with format=2 ***** </p>';
+echo '<pre>';
+echo print_r(str_word_count($str, 2));
+echo '<p> and its length (by words) is '. str_word_count($str) .' </p>';
+echo '</pre>';
+echo '<p> ▶ after implement str_word_count with format=1 and chars ***** </p>';
+echo '<pre>';
+echo print_r(str_word_count($str, 2, "/+="));
+$size = str_word_count($str, 2, "/+=");
+echo '<p> and its length (by words) is '. count($size) .' </p>';
+echo '</pre>';
+// ========= END =================
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -115,32 +197,8 @@ function render_new_names($names){
     ?>
 </head>
 <body>
-<table>
-  <tr>
-    <th>Employee Name</th>
-    <th>Salary</th>
-  </tr>
-  <?php
-    $new_arr = get_new_name($names);
-  if($names != $new_arr){
-      for($i = 0; $i<count($names); $i++){
-        echo '<tr>';
-        echo '<td>' . $new_arr[$i] . '</td>';
-        echo '<td>' . $salaries[$i] . '</td>';
-        echo '</tr>';
-      }
-  }else{
-      for($i = 0; $i<count($names); $i++){
-        echo '<tr>';
-        echo '<td>' . $names[$i] . '</td>';
-        echo '<td>' . $salaries[$i] . '</td>';
-        echo '</tr>';
-  }
-}
-  ?>
- 
-</table>
-<form action="" method="get" class="form-example">
+
+<!-- <form action="" method="get" class="form-example">
   <div class="form-example">
     <label for="oldName">Enter the old name: </label>
     <input type="text" name="oldName" id="old-name" required >
@@ -155,6 +213,6 @@ function render_new_names($names){
   <?php 
     
   ?>
-</form>
+</form> -->
 </body>
 </html>
